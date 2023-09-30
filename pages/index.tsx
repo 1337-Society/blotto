699cbc2d1622b1ba073bf6657a0deea75705429d
Anselm Hook <anselm@gmail.com>
2023-09-29 17:43:28 -0700

    initial battlefields art

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,35 +66,163 @@ Crystal Expanse
 */
 
 export const battles: BattleProps[] = [
-  {
-    title: 'The Capital',
-    art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
-    text: 'Before you is the capital city of the imperum. Yours if you can rally the charge.',
-    href: 'https://wikipedia.com/famous_battle',
-    value: 7,
-  },
-  {
-    title: 'Stormhold Citadel',
-    art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
-    text: 'Before you is the capital city of the imperum. Yours if you can rally the charge.',
-    href: 'https://wikipedia.com/famous_battle',
-    value: 4,
-  },
-  {
-    title: 'The Snowfields',
-    art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
-    text: 'Before you is the capital city of the imperum. Yours if you can rally the charge.',
-    href: 'https://wikipedia.com/famous_battle',
-    value: 3,
-  },
-  {
-    title: 'Dark Forest',
-    art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
-    text: 'Before you is the capital city of the imperum. Yours if you can rally the charge.',
-    href: 'https://wikipedia.com/famous_battle',
-    value: 3,
-  },
-];
+    {
+        title: 'The Capital',
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        text: 'Before you rise the spires of the capital city of the Imperium. Yours if you can rally the charge.',
+        art: "/images/the_capitol.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Crimson Cliffs",
+        text: "A narrow gorge, its red rock walls towering high above, where every step taken stains the earth with blood.",
+        art: "/images/crimson_cliffs.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Ironclad Arena",
+        text: "A colossal steel arena, mechanized and ever-changing, where combatants fight not only each other but the very ground beneath their feet.",
+        art: "/images/ironclad_arena.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Azure Shores",
+        text: "A serene coastal battlefield with crystal-clear waters, where the beauty of nature contrasts starkly with the brutality of combat.",
+        art: "/images/azure_shores.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Starfall Summit",
+        text: "A mountaintop battlefield under a celestial shower, where fallen stars grant power to those who can seize them.",
+        art: "/images/starfall_summit.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Crystal Expanse",
+        text: "A crystalline battleground where reflective surfaces distort reality and create a kaleidoscope of chaos for those who fight within.",
+        art: "/images/crystal_expanse.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Enchanted Glade",
+        text: "A magical forest clearing where the trees whisper secrets and the flora itself defends against intruders.",
+        art: "/images/enchanted_glade.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+]
+
+/*
+
+    {
+        "title": "Mystic Abyss",
+        text: "A swirling vortex of arcane energy, where the laws of reality bend and magic runs rampant.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+
+    {
+        "title": "Ruinheart Canyon",
+        text: "A desolate chasm filled with the crumbling remnants of ancient civilizations, where history's ghosts watch over the battles.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Stormhold Citadel",
+        text: "A formidable fortress atop a jagged peak, surrounded by perpetual thunderstorms that mask the clash of armies in a shroud of chaos.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Shadowed Peaks",
+        text: "A labyrinthine network of dimly lit caves and tunnels, where ambushes are as common as the shadows that conceal them.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Emberwilds",
+        text: "A fiery wasteland, where rivers of molten lava and flaming trees create an unforgiving hellscape for those who dare to tread.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Echoing Abyss",
+        text: "A bottomless chasm whose walls seem to absorb sound, creating an eerie silence broken only by the screams of those who fall into its depths.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+
+    {
+        "title": "Celestial Plateau",
+        text: "A lofty plateau where the heavens meet the earth, where celestial forces intervene as the mortal realm becomes their battleground.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Obsidian Bastion",
+        text: "A towering fortress carved from the darkest obsidian, its impenetrable walls housing secrets as black as its stone.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+
+    {
+        "title": "Inferno Bluffs",
+        text: "A landscape perpetually ablaze, where rivers of fire cut through jagged cliffs, turning the battlefield into a searing furnace.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Frostbite Gorge",
+        text: "A frozen crevasse where icy winds howl relentlessly, turning the very air to frost and freezing the souls of those who dare to cross.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Thunderclap Ridge",
+        text: "A rocky outcrop in a perpetual thunderstorm, where lightning strikes illuminate the chaos of battle.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Bonechill Vale",
+        text: "A frozen wasteland where ancient bones protrude from the ice, and the cold saps the strength of all who enter.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Serpent's Nest",
+        text: "A treacherous swamp filled with hidden dangers, where venomous creatures and deadly plants lie in wait.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+    {
+        "title": "Lavaforge Spire",
+        text: "A towering spire rising from a sea of lava, where the heat is only matched by the intensity of the battles fought there.",
+        art: "/images/dao_maximalist_The_battle_of_Stormhold_Citadel_7e9d32ca-5495-48bf-938d-972a08f04b8f.png",
+        href: 'https://wikipedia.com/famous_battle',
+        value: 7,
+    },
+
+*/
 
 /*
 <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
@@ -130,7 +258,8 @@ const Widget = () => {
   - make the progress bar actually alive
 
 - contract interaction
- - make stake do something
+  - make stake do something
+  - show where you actually staked stuff
   - reflect the contract state (play, or outcome)
   - show the actual live number of soldiers per battle
   - withdraw button for outcome page
