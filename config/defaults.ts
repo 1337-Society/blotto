0b1eeaf75015daaef4bde2c42285baebcc4791aa
Anselm Hook <anselm@gmail.com>
2023-09-30 14:42:16 -0700

    switched to testnet

--- config/defaults.ts
+++ config/defaults.ts
@@ -1,5 +1,5 @@
 
-export const chainName = 'juno';
+export const chainName = 'junotestnet';
 export const stakingDenom = 'ujuno';
 export const feeDenom = 'ujuno';
 export const cw20ContractAddress = 'juno1j0a9ymgngasfn3l5me8qpd53l5zlm9wurfdk7r65s5mg6tkxal3qpgf5se'
