71f052f4ffc20319ef70e7d404ae2769142d38ad
Jake Hartnell <no@no-reply.no>
2023-10-10 13:56:04 -0700

    Update testnet contract address

--- config/defaults.ts
+++ config/defaults.ts
@@ -2,4 +2,4 @@ export const chainName = "junotestnet";
 export const stakingDenom = "ujunox";
 export const feeDenom = "ujunox";
 export const blottoContractAddress =
-  "juno15h3v4436nnvqac8hyt8595dhc2u6hv4n92qmsptgrd8n7p6pgfpqr0p5x0";
+  "juno1lasy75rdvxzvd5qshj4jfpnl9w9fw5puhtmsqzvvdvdwvmlsapqsgns90n";
