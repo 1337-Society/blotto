44acb4c33cc9bebd0c043ef8fb26deba7a57a3bc
Joel Smith <joelsmith.2019@gmail.com>
2023-10-20 22:51:43 -0500

    Update error message

--- contracts/cw-blotto/src/error.rs
+++ contracts/cw-blotto/src/error.rs
@@ -56,6 +56,6 @@ pub enum ContractError {
     #[error("{0}")]
     PaymentError(#[from] PaymentError),
 
-    #[error("Start time must be in the future (in nanos); current timestamp: {now}, got timestamp: {start_time}")]
+    #[error("Start time must be in the future; current block timestamp (in seconds): {now}, received start timestamp (in seconds): {start_time}")]
     InvalidStartTime { now: u64, start_time: u64 },
 }
